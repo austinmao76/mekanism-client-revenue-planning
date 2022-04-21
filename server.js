780fb3cebceda51713f5122b09d4aab030d45de0
austinmao76 <austinmao@eleveninc.com>
2022-04-21 00:39:36 -0700

    routers and controller structure

--- server.js
+++ server.js
@@ -3,20 +3,41 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+//db and authenticate
+import connectDB from './db/connect.js'
+
+//routers
+
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobRoutes.js'
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req, res) => {
-	throw new Error(error)
 	res.send('welcome')
 })
+
+app.use(express.json())
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-	console.log(`server is listening on port ${port}...`)
-})
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL)
+		app.listen(port, () => {
+			console.log(`server is listening on port ${port}...`)
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
