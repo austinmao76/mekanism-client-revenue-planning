254dd0213a83616c367c8948c97fc9caa09e8651
austinmao76 <austinmao@eleveninc.com>
2022-04-21 09:35:57 -0700

    nested pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Dashboard, Register, Landing, Error } from './pages'
+import { Register, Landing, Error, ProtectedRoute } from './pages'
+import {
+	Stats,
+	SharedLayout,
+	Profile,
+	AddJob,
+	AllJobs,
+} from './pages/dashboard'
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Dashboard />} />
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<SharedLayout />
+						</ProtectedRoute>
+					}>
+					<Route index element={<Stats />} />
+					<Route path='all-jobs' element={<AllJobs />}></Route>
+					<Route path='add-job' element={<AddJob />}></Route>
+					<Route path='profile' element={<Profile />}></Route>
+				</Route>
 				<Route path='/register' element={<Register />} />
 				<Route path='/landing' element={<Landing />} />
 				<Route path='/*' element={<Error />} />
